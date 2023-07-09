1e71647ea8594d204992147411478e27d7c3b3d5
Wayne Otido <otidowayne@gmail.com>
2023-07-10 00:49:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
+  
+}
+
+function reverseString(word) {
+  return word.split("").reverse().join("");
 }
 
 /* 
